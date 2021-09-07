e1b81083941702a11011d87fe175e91180dfc97d
Ivan Usov <ivan.usov@psi.ch>
2021-09-07 16:50:16 +0200

    Add PBPS122 and PBPS138

---
+++ PBPS138/update.py
@@ -0,0 +1,22 @@
+import json
+
+from cam_server import PipelineClient
+pc = PipelineClient("http://sf-daqsync-01:8889")
+
+pipeline_name = "SAROP21-PBPS138_proc"
+instance_name = pipeline_name + "1"
+
+# update config
+with open("/sf/photo/pipeline_cam_server/PBPS138/SAROP21-PBPS138_proc.json") as config_file:
+    config = json.load(config_file)
+
+pc.save_pipeline_config(pipeline_name, config)
+
+# update process func
+filename = "/sf/photo/pipeline_cam_server/functions/pbps.py"
+try:
+    pc.set_function_script(instance_name, filename)
+except:
+    pc.upload_user_script(filename)
+
+pc.stop_instance(instance_name)
